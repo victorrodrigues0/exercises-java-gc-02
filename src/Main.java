52c357094068096b0963abb2c6e1993d0dce503f
Victor Rodrigues <victorrodrigues.ofc00@gmail.com>
2026-06-05 04:37:16 -0300

    new: create movie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+public class Main {
+    static void main() {
+    Movie movie1 = new Movie();
+    Movie movie2 = new Movie();
 
+    movie1.name = "Pecadores";
+    movie1.releaseYear = 2025;
+    movie1.evaluateMovie(9);
+    movie1.evaluateMovie(8);
+
+    movie1.showMovie();
+    }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,6 +1,24 @@
 public class Movie {
     String name;
-    int releaseYear, review, allReviews,timeMovie;
+    int releaseYear, review, allReviews = 0,movieDuration, countReview = 0;
     boolean isInsidePremiumPlan;
 
+    void showMovie () {
+        System.out.println("Nome do filme: " + this.name);
+        System.out.println("Data de lançamento: " + this.releaseYear);
+        System.out.println("Sua nota do filme: " + this.review);
+        System.out.println("Nota geral do filme: " + this.allReviews / countReview);
+        System.out.println("Duração do filme: " + this.movieDuration );
+        System.out.println(this.isInsidePremiumPlan ? "Está no plano Premium? Sim." : "Está no plano Premium? Não.");
+    }
+
+    void evaluateMovie (int score) {
+        if (score < 0 || score > 10) {
+            System.out.println("A avaliação deve estar entre 0 - 10.\n");
+            return;
+        }
+        this.review = score;
+        allReviews += this.review;
+        this.countReview++;
+    }
 }
